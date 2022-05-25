c226a6cc7d22f83ad726941005e4b6b0b44b761a
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-25 22:23:25 +0300

    dz16
    dz16

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 1;
+            int counter = 0;
+
+            for(int i = 100; i < 1000; i++)
+            {
+                counter++;
+            }
+
+            Console.WriteLine($"Количество трехзначных натуральных чисел, которые кратны {N}  = {counter}");
+
+        }
+    }
+}
